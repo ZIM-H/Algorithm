b5740593f26f87afa79bfadcd4c32e77a0ca2a08
Jimin Hong <57134670+ZIM-H@users.noreply.github.com>
2023-12-28 23:03:27 +0900

    [Silver I] Title: 정수 삼각형, Time: 292 ms, Memory: 27172 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[n][n];
+        int[][] value = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < i + 1; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        value[0][0] = arr[0][0];
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                if (j == 0) {
+                    value[i][j] = value[i - 1][j] + arr[i][j];
+                    continue;
+                } else if (i == j) {
+                    value[i][j] = value[i - 1][j - 1] + arr[i][j];
+                    continue;
+                }
+
+                value[i][j] = Math.max(value[i - 1][j - 1] + arr[i][j], value[i - 1][j] + arr[i][j]);
+            }
+        }
+
+        Arrays.sort(value[n-1]);
+        System.out.println(value[n-1][n-1]);
+    }
+}
