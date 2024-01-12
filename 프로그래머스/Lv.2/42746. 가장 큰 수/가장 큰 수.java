46220553851ca648bab2b19fee344554993e621f
Jimin Hong <57134670+ZIM-H@users.noreply.github.com>
2024-01-12 22:45:51 +0900

    [level Lv.2] Title: 가장 큰 수, Time: 3.45 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] nums = new String[numbers.length];
+        for(int i=0; i<numbers.length; i++){
+            nums[i] = Integer.toString(numbers[i]);
+        }
+        
+        Arrays.sort(nums, new Comparator<String>(){
+            public int compare(String o1, String o2){
+                return (o2+o1).compareTo(o1+o2);
+            }
+        });
+        
+        if(nums[0].equals("0")) return "0";
+        
+        for(int i=0; i<nums.length; i++){
+            answer += nums[i];
+        }
+        
+        return answer;
+    }
+}
