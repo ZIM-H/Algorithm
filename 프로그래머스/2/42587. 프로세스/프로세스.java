4fd2d1b13d2e3df2ecfb230c879c2b1455c5a537
Jimin Hong <57134670+ZIM-H@users.noreply.github.com>
2024-02-01 15:20:52 +0900

    [level 2] Title: 프로세스, Time: 0.62 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -11,24 +11,24 @@ class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
         
-        PriorityQueue<Integer> num = new PriorityQueue<>(Collections.reverseOrder());
         Queue<Word> q = new ArrayDeque<>();
-        
         for(int i=0; i<priorities.length; i++){
-            num.add(priorities[i]);
             q.add(new Word(i, priorities[i]));
         }
         
+        Arrays.sort(priorities);
+        int index = priorities.length-1;
         while(true){
             Word now = q.poll();
             
-            if(num.peek() == now.value){
+            if(priorities[index] == now.value){
                 if(now.idx == location) {
                     answer++;
                     break;
                 }
                 answer++;
-                num.poll();
+                index--;
+                
                 continue;
             }
             q.add(now);
