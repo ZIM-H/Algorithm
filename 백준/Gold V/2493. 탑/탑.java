f6120524d46723d7342aede88b5cfbe4eb519506
Jimin Hong <57134670+ZIM-H@users.noreply.github.com>
2024-02-15 22:48:49 +0900

    [Gold V] Title: 탑, Time: 700 ms, Memory: 87196 KB -BaekjoonHub

--- 백준/Gold V/2493. 탑/탑.java
+++ 백준/Gold V/2493. 탑/탑.java
@@ -1,45 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        Stack<Tower> s = new Stack<>();
-        StringBuilder sb = new StringBuilder();
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=1; i<=n; i++){
-            Tower now = new Tower(i, Integer.parseInt(st.nextToken()));
-
-            // 스택이 비어있지않고, 현재 탑보다 작은 경우 다 지움
-            while(!s.isEmpty() && s.peek().height < now.height){
-                s.pop();
-            }
-
-            // 스택 비어있는 경우 비교 대상이 없으므로 0
-            if(s.isEmpty()) sb.append("0 ");
-
-            // 현재보다 큰 탑의 인덱스 번호
-            else sb.append(s.peek().idx+" ");
-
-            // 현재 탑 스택에 넣기
-            s.push(now);
-        }
-
-        System.out.println(sb.toString());
-
-    }
-
-    static class Tower {
-        int idx, height;
-
-        public Tower(int idx, int height){
+    static class Top{
+        int idx;
+        int height;
+        public Top(int idx, int height){
             this.idx = idx;
             this.height = height;
         }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        Deque<Top> d = new ArrayDeque<>();
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            Top now = new Top(i+1, Integer.parseInt(st.nextToken()));
+            while (!d.isEmpty() && now.height >= d.peekLast().height) d.pollLast();
+
+            if(d.isEmpty()) sb.append(0).append(" ");
+            else sb.append(d.peekLast().idx).append(" ");
+
+            d.add(now);
+        }
+        System.out.println(sb);
     }
 }
