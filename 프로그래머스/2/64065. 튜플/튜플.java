3b5549b9f3f98880ddb4d4e7d04cc9281b3b1927
Jimin Hong <57134670+ZIM-H@users.noreply.github.com>
2024-02-16 17:48:05 +0900

    [level 2] Title: 튜플, Time: 0.89 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,28 +2,20 @@ import java.util.*;
 class Solution {
     public int[] solution(String s) {
         String[] num = s.split(",|\\{|\\}");
-        int[][] cnt = new int[100009][2];
+        HashMap<String, Integer> hm = new HashMap<>();
         
-        int idx = 0;
         for(int i=0; i<num.length; i++){
             if(num[i].equals("")) continue;
-            int n = Integer.parseInt(num[i]);
-            if(cnt[n][1] == 0) {
-                idx++;
-                cnt[n][0] = n;
-            }
-            cnt[n][1]++;
+            hm.put(num[i], hm.getOrDefault(num[i], 0)+1);
         }
+        List<String> list = new ArrayList<>(hm.keySet());
+        Collections.sort(list, (v1, v2) -> hm.get(v2)-hm.get(v1));
         
-        Arrays.sort(cnt, new Comparator<int[]>(){
-            public int compare(int[] s1, int[] s2){
-                return s2[1]-s1[1];
-            }
-        });
-        
-        int[] answer = new int[idx];
-        for(int i=0; i<idx; i++){
-            answer[i] = cnt[i][0];
+        int[] answer = new int[hm.size()];
+        int idx = 0;
+        for(String str : list){
+            answer[idx] = Integer.parseInt(str);
+            idx++;
         }
         
         return answer;
