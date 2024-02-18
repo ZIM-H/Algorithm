bbef08215765a8508a65c559521e8aa24c364ab3
Jimin Hong <57134670+ZIM-H@users.noreply.github.com>
2024-02-18 22:31:24 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,18 +1,12 @@
 class Solution {
-    static int answer;
     public int solution(int a, int b, int n) {
-        answer = 0;
+        int answer = 0;
         
-        change(n, a, b);
+        while(n >= a){
+            answer += (n/a) * b;
+            n = ((n/a) * b) + (n % a);
+        }
         
         return answer;
-    }
-    
-    static void change(int coke, int bottle, int cnt){
-        if(coke / bottle < 1) return;
-        
-        answer += (coke / bottle)*cnt;
-        
-        change(((coke / bottle)*cnt) + (coke%bottle), bottle, cnt);
     }
 }
