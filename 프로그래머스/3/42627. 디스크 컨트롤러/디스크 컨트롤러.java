9f52184e86c11fbd92ccaa0d8c4c11f898d6f91f
Jimin Hong <57134670+ZIM-H@users.noreply.github.com>
2024-03-04 14:14:43 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.98 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,27 +2,31 @@ import java.util.*;
 class Solution {
     public int solution(int[][] jobs) {
         int answer = 0;
-        PriorityQueue<int[]> req = new PriorityQueue<>((o1, o2) -> o1[0] == o2[0]? o1[1] - o2[1] : o1[0] - o2[0]);
+        Arrays.sort(jobs, (o1, o2) -> o1[0] == o2[0]? o1[1] - o2[1] : o1[0] - o2[0]);
+        // PriorityQueue<int[]> req = new PriorityQueue<>((o1, o2) -> o1[0] == o2[0]? o1[1] - o2[1] : o1[0] - o2[0]);
         PriorityQueue<int[]> res = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
         
-        for(int[] j : jobs){
-            req.add(j);
-        }
+        // for(int[] j : jobs){
+        //     req.add(j);
+        // }
         
         int now = 0;
+        int idx = 0;
         int[] ele;
-        while(!req.isEmpty() || !res.isEmpty()){
-            while(!req.isEmpty()){
-                ele = req.peek();
+        while(idx < jobs.length || !res.isEmpty()){
+            while(idx < jobs.length){
+                ele = jobs[idx];
                 if(ele[0] > now) break;
                 
-                res.add(req.poll());
+                res.add(ele);
+                idx++;
             }
             
             if(res.isEmpty()){
-                ele = req.poll();
+                ele = jobs[idx];
                 now = ele[0] + ele[1];
                 answer += ele[1];
+                idx++;
             }
             else{
                 ele = res.poll();
