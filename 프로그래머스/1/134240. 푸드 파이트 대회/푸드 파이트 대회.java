6a195034d9717373fa885dbb3a6b49cc1fd6f955
Jimin Hong <57134670+ZIM-H@users.noreply.github.com>
2024-03-05 14:55:59 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 8.58 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<food.length; i++){
+            for(int j=0; j<food[i]/2; j++){
+                sb.append(i);
+            }
+        }
+        
+        answer += sb + "0";
+        answer += sb.reverse();
+        return answer;
+    }
+}
