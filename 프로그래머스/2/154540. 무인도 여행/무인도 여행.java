50e3898565fcceb37ecb06d35a4af9db7b5ed888
Jimin Hong <57134670+ZIM-H@users.noreply.github.com>
2024-03-05 14:43:21 +0900

    [level 2] Title: 무인도 여행, Time: 0.85 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -0,0 +1,74 @@
+import java.util.*;
+class Solution {
+    static int[] dr = {-1,1,0,0};
+    static int[] dc = {0,0,1,-1};
+    static char[][] map;
+    static boolean[][] visited;
+    static class Pos{
+        int r;
+        int c;
+        public Pos(int r, int c){
+            this.r = r;
+            this.c = c;
+        }
+    }
+    public int[] solution(String[] maps) {
+        map = new char[maps.length][];
+        
+        for(int i=0; i<maps.length; i++){
+            map[i] = maps[i].toCharArray();
+        }
+        
+        List<Integer> list = new ArrayList<>();
+        visited = new boolean[map.length][map[0].length];
+        int flag = 0;
+        for(int i=0; i<map.length; i++){
+            for(int j=0; j<map[i].length; j++){
+                if(map[i][j] != 'X' && !visited[i][j]){
+                    int n = cal(i,j);
+                    list.add(n);
+                    flag = flag == 0 ? 1 : flag;
+                }
+            }
+        }
+        
+        int[] answer;
+        
+        if(flag == 0){
+            answer = new int[1];
+            answer[0] = -1;
+        }
+        
+        else{
+            Collections.sort(list);
+            answer = new int[list.size()];
+            for(int i=0; i<list.size(); i++){
+                answer[i] = list.get(i);
+            }
+        }
+        return answer;
+    }
+    
+    static int cal(int i, int j){
+        Queue<Pos> q = new ArrayDeque<>();
+        visited[i][j] = true;
+        q.add(new Pos(i,j));
+        int value = 0;
+        
+        while(!q.isEmpty()){
+            Pos now = q.poll();
+            value += map[now.r][now.c] - '0';
+            
+            for(int d=0; d<4; d++){
+                int nr = now.r + dr[d];
+                int nc = now.c + dc[d];
+                if(nr >= 0 && nc >= 0 && nr < map.length && nc < map[0].length && !visited[nr][nc] && map[nr][nc] != 'X'){
+                    visited[nr][nc] = true;
+                    q.add(new Pos(nr, nc));
+                }
+            }
+        }
+        
+        return value;
+    }
+}
