8e9991b6abfc05a41fbab9665145c842f7e189c6
Jimin Hong <57134670+ZIM-H@users.noreply.github.com>
2024-03-12 11:54:29 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.02 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class Solution {
+    static List<List<Integer>> graph;
+    static boolean[] visited;
+    
+    public int solution(int n, int[][] wires) {
+        int answer = Integer.MAX_VALUE;
+        graph = new ArrayList<>();
+        for(int i=0; i<=n; i++){
+            graph.add(new ArrayList<>());
+        }
+        
+        for(int[] wire : wires){
+            graph.get(wire[0]).add(wire[1]);
+            graph.get(wire[1]).add(wire[0]);
+        }
+        
+        int count;
+        for(int[] wire: wires){
+            visited = new boolean[n+1];
+            
+            graph.get(wire[0]).remove(Integer.valueOf(wire[1]));
+            graph.get(wire[1]).remove(Integer.valueOf(wire[0]));
+            
+            count = dfs(wire[0]);
+            answer = Math.min(answer, Math.abs((n-count)-count));
+            
+            graph.get(wire[0]).add(wire[1]);
+            graph.get(wire[1]).add(wire[0]);
+        }
+        return answer;
+    }
+    
+    static int dfs(int node){
+        visited[node] = true;
+        int cnt = 1;
+        
+        for(int n : graph.get(node)){
+            if(!visited[n]) cnt += dfs(n);
+        }
+        
+        return cnt;
+    }
+}
