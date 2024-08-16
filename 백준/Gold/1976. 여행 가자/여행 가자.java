4a4e128f287f2bb6400931c7d555277b8d313cf7
Jimin Hong <57134670+ZIM-H@users.noreply.github.com>
2024-08-16 18:01:55 +0900

    [Gold IV] Title: 여행 가자, Time: 156 ms, Memory: 19100 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -4,31 +4,18 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void union(int x, int y){
-        x = find(x);
-        y = find(y);
-
-        if(x > y) parent[y] = x;
-        else parent[x] = y;
-    }
-
-    public static int find(int x){
-        if(x == parent[x]) return x;
-        return find(parent[x]);
-    }
     static int[] parent;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-        parent = new int[n+1];
 
+        parent = new int[n+1];
         for(int i=1; i<=n; i++){
             parent[i] = i;
         }
 
         StringTokenizer st;
-
         for(int i=1; i<=n; i++){
             st = new StringTokenizer(br.readLine());
             for(int j=1; j<=n; j++){
@@ -40,13 +27,25 @@ public class Main {
 
         st = new StringTokenizer(br.readLine());
         int start = find(Integer.parseInt(st.nextToken()));
-        for(int i=0; i<m-1; i++){
-            if(start != find(Integer.parseInt(st.nextToken()))){
+        for(int i=0; i< m-1; i++){
+            if(find(Integer.parseInt(st.nextToken())) != start) {
                 System.out.println("NO");
                 return;
             }
         }
-        System.out.println("YES");
 
+        System.out.println("YES");
+    }
+    public static int find(int x){
+        if(parent[x] == x) return x;
+
+        return find(parent[x]);
+    }
+
+    public static void union(int x, int y){
+        x = find(x);
+        y = find(y);
+        if(x > y) parent[y] = x;
+        else parent[x] = y;
     }
 }
