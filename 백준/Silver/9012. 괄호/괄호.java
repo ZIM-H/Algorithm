8ce2df53ea3dabdd6f1af5d9680592be2919e6ec
Jimin Hong <57134670+ZIM-H@users.noreply.github.com>
2024-09-25 10:41:26 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -8,26 +8,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n  = Integer.parseInt(br.readLine());
-        Stack<Integer> s;
 
         for(int i=0; i<n; i++) {
             String ps = br.readLine();
-            s = new Stack<>();
-            int flag = 0;
-            for (char c : ps.toCharArray()) {
-                if (c == ')') {
-                    if (!s.isEmpty()) s.pop();
-                    else {
-                        flag = 1;
-                        System.out.println("NO");
+            int num = 0;
+            for(char c : ps.toCharArray()){
+                if(c == '(') num++;
+                else {
+                    if(num == 0) {
+                        num++;
                         break;
                     }
-                } else s.push(0);
+                    else num--;
+                }
             }
-            if(flag == 1) continue;
 
-            String answer = s.size() == 0 ? "YES" : "NO";
+            String answer = num == 0 ? "YES" : "NO";
             System.out.println(answer);
+
         }
     }
 }
