98504356a1d3422b6561d9c35070312b826ea704
Jimin Hong <57134670+ZIM-H@users.noreply.github.com>
2024-10-18 14:45:05 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 1712 ms, Memory: 69836 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -1,30 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
-    static int n, m, desR, desC;
-    static int[][] map, tmp;
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
-    static boolean[][] visited;
-
-    public static class Pos {
-        int r = 0;
-        int c = 0;
-        int value = 0;
-
-        public Pos(int r, int c, int value) {
+class Main {
+    static int[] dr = {-1,1,0,0};
+    static int[] dc = {0,0,-1,1};
+    static int n,m;
+    static int[][] map, answer;
+    static class Pos {
+        int r , c, cnt;
+        public Pos(int r, int c, int cnt){
             this.r = r;
             this.c = c;
-            this.value = value;
+            this.cnt = cnt;
         }
     }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -32,52 +26,52 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         map = new int[n][m];
-        tmp = new int[n][m];
+        answer = new int[n][m];
 
-        desR = 0;
-        desC = 0;
-        for (int i = 0; i < n; i++) {
+
+        Pos start = null;
+        for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
+            Arrays.fill(answer[i], -1);
+            for(int j=0; j<m; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == 2) {
-                    desR = i;
-                    desC = j;
+                if(map[i][j] == 2) {
+                    start = new Pos(i,j,0);
+                    answer[i][j] = 0;
                 }
-                else if (map[i][j] == 1) tmp[i][j] = -1;
+                else if(map[i][j] == 0) answer[i][j] = 0;
             }
         }
-        visited = new boolean[n][m];
-        bfs(desR, desC);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(tmp[i][j]+" ");
+        bfs(start);
+
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                System.out.print(answer[i][j]+" ");
             }
             System.out.println();
         }
     }
 
-
-
-    private static void bfs(int i, int j) {
+    public static void bfs(Pos start){
         Queue<Pos> q = new ArrayDeque<>();
-        q.add(new Pos(i, j, 0));
-        visited[i][j] = true;
+        boolean[][] visited = new boolean[n][m];
+        q.add(new Pos(start.r, start.c, 0));
+        visited[start.r][start.c] = true;
 
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()){
             Pos now = q.poll();
 
-            for (int d = 0; d < 4; d++) {
+            for(int d=0; d<4; d++){
                 int nr = now.r + dr[d];
                 int nc = now.c + dc[d];
-                if (nr >= 0 && nc >= 0 && nr < n && nc < m && !visited[nr][nc] && map[nr][nc] == 1) {
-                    q.add(new Pos(nr, nc, now.value + 1));
+                if(nr >= 0 && nc >= 0 && nr < n && nc < m && map[nr][nc] == 1 && !visited[nr][nc]){
+                    q.add(new Pos(nr, nc, now.cnt + 1));
                     visited[nr][nc] = true;
-                    tmp[nr][nc] = now.value + 1;
+                    answer[nr][nc] = now.cnt + 1;
                 }
             }
         }
-
     }
 }
