f051e53e6f11158e6677e77034d6ca288915d86c
Jimin Hong <57134670+ZIM-H@users.noreply.github.com>
2024-10-26 21:30:26 +0900

    [Gold IV] Title: 해킹, Time: 1052 ms, Memory: 166748 KB -BaekjoonHub

---
+++ 백준/Gold/10282. 해킹/해킹.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static class Node implements Comparable<Node>{
+        int n, cost;
+        public Node(int n, int cost){
+            this.n = n;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o){
+            return this.cost - o.cost;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        while(T-- > 0){
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            List<Node>[] graph = new ArrayList[n+1];
+            for(int i=1; i<=n; i++){
+                graph[i] = new ArrayList<>();
+            }
+
+            for(int i=0; i<d; i++){
+                st = new StringTokenizer(br.readLine());
+                int s = Integer.parseInt(st.nextToken());
+                int e = Integer.parseInt(st.nextToken());
+                int cost = Integer.parseInt(st.nextToken());
+                graph[e].add(new Node(s, cost));
+            }
+
+            int[] dist = new int[n+1];
+            Arrays.fill(dist, 987654321);
+            dist[c] = 0;
+
+            PriorityQueue<Node> q = new PriorityQueue<>();
+            q.add(new Node(c, 0));
+
+            while (!q.isEmpty()){
+                Node now = q.poll();
+
+                for(Node next : graph[now.n]){
+                    if(dist[next.n] > dist[now.n]+next.cost){
+                        dist[next.n] = dist[now.n]+next.cost;
+                        q.add(new Node(next.n, dist[next.n]));
+                    }
+                }
+            }
+            int cnt = 0, max = 0;
+            for(int i : dist){
+                if(i != 987654321) {
+                    cnt++;
+                    max = Math.max(max, i);
+                }
+            }
+            System.out.println(cnt+" "+max);
+
+        }
+
+
+    }
+}
